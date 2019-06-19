489c27a028ee291af574bbe9322611d6d7c741f6
David <gelpiorama@gmail.com>
2019-06-19 20:05:03 +0200

    main sin la historia de Rick

--- src/main/java/intercepting/App.java
+++ src/main/java/intercepting/App.java
@@ -10,29 +10,6 @@ public final class App {
 
     public static void main(String[] args) {
 
-        /**
-         * Rick es mou per diferents realitats paraleles 
-         * disparant la seva pistola de portals.
-         * 
-         * La pistola de portals de Rick necessita la autenticacio
-         * de l'usuari com a propietari de la pistola abans de ser 
-         * disparada i la comprobació de que l'usuari disposa de
-         * acces a la realitat a la que preten accedir.
-         * 
-         * Filtres:
-         *  - Autenticacio
-         *  - Acces
-         * 
-         * Target:
-         *  - Disparar
-         * 
-         * La pistola necessita una cadena de filtres
-         * controlada per un administrador de filtres 
-         * que posa la execucio del tret despres de 
-         * chequear els filtres.
-         * 
-         */
-
         ProgramadorTasques programadorTasques = new ProgramadorTasques(new BlockChain());
         programadorTasques.setTasca(new Autenticacio());
         programadorTasques.setTasca(new Credit());
